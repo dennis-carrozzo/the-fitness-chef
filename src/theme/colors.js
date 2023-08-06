9f8a0b0567d91b7c85e34a3a957144b73ec3a1f6
denniswebdel <denniswebdel@gmail.com>
2023-08-06 16:40:56 +0100

    build(ui): Add website color palette

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -6,17 +6,28 @@ const palette = {
     main: '#795548'
   },
   accent: {
-    main: '#d2b48c'
+    main: '#d2b48c',
+    dark: '#937d62',
+    light: '#dbc3a3'
   },
   accent2: {
-    main: '#698996'
+    main: '#698996',
+    dark: '#495f69',
+    light: '#87a0ab'
   },
   dark: {
-    main: '#000000'
+    main: '#000000',
+    light: '#1a1a1a'
   },
   light: {
-    main: '#FFFFFF'
+    main: '#FFFFFF',
+    dark: '#b2b2b2'
   }
 }
+palette.text = {}
+palette.text.primary = palette.light.main
+palette.text.secondary = palette.light.dark
+palette.text.dark = palette.dark.main
+palette.text.darksecondary = palette.dark.light
 
 export default palette
